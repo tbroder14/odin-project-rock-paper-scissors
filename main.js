e6a83bea103add5e3f2e7157b4aaf8835ffd4077
tbroder14 <111935914+tbroder14@users.noreply.github.com>
2023-01-16 10:47:37 -0600

    RPS V2 with UI

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// option one: rock, paper, scissors in a list; random number between 0-2
+// option two: 
+
 function getComputerChoice() {
     const rockGame = ["Rock", "Paper", "Scissors"];
     const random = rockGame[Math.floor(Math.random() * rockGame.length)];
@@ -5,37 +8,68 @@ function getComputerChoice() {
     return allLetters;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    
     if (playerSelection == computerSelection) {
-        return "Tied";
+        div.append('Tied.')
+        div.appendChild(document.createElement('br'));
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You win!";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You win"; 
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You win!";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You lose!"; 
+        winTally = 1 + winTally
+        div.append(`You win! ${playerSelection} beats ${computerSelection}`)
+        div.appendChild(document.createElement('br'));
+        document.getElementById('win-tally').innerHTML = winTally
+        return winTally
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You lose!";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You lose!"; 
+        loseTally = 1 + loseTally
+        div.append(`You lose! ${computerSelection} beats ${playerSelection}`)
+        div.appendChild(document.createElement('br'));
+        document.getElementById('lose-tally').innerHTML = loseTally
+        return loseTally
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        winTally = 1 + winTally
+        div.append(`You win! ${playerSelection} beats ${computerSelection}`)
+        div.appendChild(document.createElement('br'));
+        document.getElementById('win-tally').innerHTML = winTally
+        return winTally
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You lose!";     
-    } else {
-        return "Nice try! You probably misspelled something or entered an incorrect answer";
+        loseTally = 1 + loseTally
+        div.append(`You lose! ${computerSelection} beats ${playerSelection}`)
+        div.appendChild(document.createElement('br'));;   
+        document.getElementById('lose-tally').innerHTML = loseTally  
+        return loseTally
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        loseTally = 1 + loseTally
+        div.append(`You lose! ${computerSelection} beats ${playerSelection}`)
+        div.appendChild(document.createElement('br'));
+        document.getElementById('lose-tally').innerHTML = loseTally
+        return loseTally
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        winTally = 1 + winTally
+        div.append(`You win! ${playerSelection} beats ${computerSelection}`)
+        div.appendChild(document.createElement('br'));
+        document.getElementById('win-tally').innerHTML = winTally
+        return winTally
     }
 }
 
-function game () {
-    for (let i = 0; i < 5; i++) {
-        const computerSelection = getComputerChoice();
-        const playerSelection = prompt("Enter either rock, paper, scissors.");
-        console.log(playerSelection);
-        console.log(computerSelection);
-        console.log(playRound(playerSelection, computerSelection))  
-        
+function checkGameScore (winTally, loseTally) {
+    if (winTally >= 5) {
+        alert('you win!')
+        rock.removeEventListener("click", rockFunction);
+        paper.removeEventListener('click', paperFunction);
+        scissors.removeEventListener('click', scissorsFunction);
+        throw new Error(); 
+    } else if (loseTally >= 5) {
+        alert('You lose!')
+        rock.removeEventListener("click", rockFunction);
+        paper.removeEventListener('click', paperFunction);
+        scissors.removeEventListener('click', scissorsFunction);
     }
+
 }
 
 game()
+
+// rock beats scissors, paper beats rock, scissors beat paper 
+
