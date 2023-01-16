b436ddc21ffcfdbb6cd2c3661f0329d9bd8b12eb
tbroder14 <111935914+tbroder14@users.noreply.github.com>
2023-01-16 17:26:18 -0600

    RPS V2 with UI

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-// option one: rock, paper, scissors in a list; random number between 0-2
-// option two: 
+const rock = document.querySelector('.rock')
+const paper = document.querySelector('.paper')
+const scissors = document.querySelector('.scissors')
+const div = document.querySelector('.div')
+let winTally = 0; 
+let loseTally = 0;
+const playerSelection = input()
+
+
+function input() {
+    rock.addEventListener("click", rockFunction);
+    paper.addEventListener('click', paperFunction);
+    scissors.addEventListener('click', scissorsFunction);
+}
 
 function getComputerChoice() {
     const rockGame = ["Rock", "Paper", "Scissors"];
@@ -57,19 +69,42 @@ function checkGameScore (winTally, loseTally) {
     if (winTally >= 5) {
         alert('you win!')
         rock.removeEventListener("click", rockFunction);
+        rock.style.opacity = "0.6"
+        rock.style.cursor = 'not-allowed'
         paper.removeEventListener('click', paperFunction);
+        paper.style.opacity = "0.6"
+        paper.style.cursor = 'not-allowed'
         scissors.removeEventListener('click', scissorsFunction);
-        throw new Error(); 
+        scissors.style.opacity = "0.6"
+        scissors.style.cursor = 'not-allowed'
+
     } else if (loseTally >= 5) {
         alert('You lose!')
         rock.removeEventListener("click", rockFunction);
+        rock.style.opacity = "0.6"
+        rock.style.cursor = 'not-allowed'
         paper.removeEventListener('click', paperFunction);
+        paper.style.opacity = "0.6"
+        paper.style.cursor = 'not-allowed'
         scissors.removeEventListener('click', scissorsFunction);
+        scissors.style.opacity = "0.6"
+        scissors.style.cursor = 'not-allowed'
     }
 
 }
 
-game()
-
-// rock beats scissors, paper beats rock, scissors beat paper 
-
+function rockFunction(playerSelection) {
+    playerSelection = rock.classList.value
+    playRound(playerSelection)
+    checkGameScore(winTally, loseTally)
+}
+function paperFunction(playerSelection) {
+    playerSelection = paper.classList.value
+    playRound(playerSelection)
+    checkGameScore(winTally, loseTally)
+}
+function scissorsFunction(playerSelection) {
+    playerSelection = scissors.classList.value
+    playRound(playerSelection)
+    checkGameScore(winTally, loseTally)
+}
